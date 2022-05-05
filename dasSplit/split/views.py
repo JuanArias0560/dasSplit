44550389bbbc14ad3a94751dbe06179ef448e976
Juan Arias <juanmanuel0560@gmail.com>
2022-05-04 21:57:16 -0500

    Se agrega la aplicacion split, se crea el archivo urls.py y forms.py  para la aplicacion y se edita el archivo views

---
+++ dasSplit/split/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages 
+
+def register_request(request):
+    if request.method == "post":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            messages.success(request,"El registro fue exitoso.")
+            return redirect("main:homepage")
+        messages.error(request,"No se pudo registrar, inserte informacion valida")
+    form=NewUserForm()
+    return render (request=request, template_name="split/register.html", context={"register_form":form})
+
+
+def homepage(request):
+    return render(request,'split/home.html')
