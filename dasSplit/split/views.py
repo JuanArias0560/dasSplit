7facfe0bc050640ec3c96fac83f45a824a65bc28
Juan Arias <juanmanuel0560@gmail.com>
2022-05-06 18:52:35 -0500

    Se agregan archivos estaticos y cambion en el home, creacion del login el estilo y cambio en el settings y url

--- dasSplit/split/views.py
+++ dasSplit/split/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.contrib import messages 
+from django.contrib.auth.forms import AuthenticationForm
 
 def register_request(request):
+
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
@@ -12,8 +14,29 @@ def register_request(request):
             messages.success(request,"El registro fue exitoso.")
             return redirect("split:homepage")
         messages.error(request,"No se pudo registrar, inserte informacion valida")
+        redirect("split:register")
     form=NewUserForm()
     return render (request=request, template_name="split/register.html", context={"register_form":form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                messages.info(request,f'Bienvenido de nuevo {username}')
+                return redirect("split:homepage")
+            else:
+                messages.error(request,"Usuario o contraseña invalidos")
+        else:
+            messages.error(request,"Usuario o contraseña invalidos")
+    form =AuthenticationForm()
+    return render(request=request, template_name="split/login.html", context={"login_form":form})
+    
 
 
 def homepage(request):
